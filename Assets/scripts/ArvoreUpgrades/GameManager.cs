58d63eb85444cf368e3056510d91c103f2bcead9
Unknown <gabriel.tex.galam@gmail.com>
2018-01-27 00:05:06 -0200

    ArvoreNotoriedade
    Ta feito a arvore de notoriedade

---
+++ Assets/scripts/ArvoreUpgrades/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	public GameObject meme;
+
+	public int notoriety;
+
+	// Use this for initialization
+	void Awake () {
+
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/ArvoreUpgrades/Notoriedade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notoriedade : MonoBehaviour {
+
+	public GameManager manager;
+	GameObject meme;
+
+	public int atk;
+	public int notoriety;
+
+	// Use this for initialization
+	void Awake () {
+
+		meme = manager.meme;
+	}
+	
+	// Update is called once per frame
+	void Start () {
+
+		meme.GetComponent<MemeAtributtes> ().atk += atk;
+		manager.notoriety += notoriety;
+	}
+}
