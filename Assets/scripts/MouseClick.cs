ab059ec8f9421f74cead49dfc757d88afa5786bc
Chieppe <chieppe2142@gmail.com>
2018-01-27 04:38:17 -0200

    Clicar num pais estoura PopUps e faz os efeitos

---
+++ Assets/scripts/MouseClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour {
+
+    RaycastHit Rayhit;
+
+    private CountryAtributtes CountryClick;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        if (Input.GetButtonDown("Fire1")) {
+            if (Physics.Raycast(ray, out Rayhit, 100)) {
+                CountryClick = Rayhit.collider.gameObject.GetComponent<CountryAtributtes>();
+                if (CountryClick.HasPopUp) {
+                    CountryClick.transform.GetChild(0).GetComponent<PopClick>().Detonate();
+                }
+            }
+        }
+	}
+}
---
+++ Assets/scripts/PopClick.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopClick : MonoBehaviour {
+
+    public int PopType;
+    private PopUp PU;
+
+    private CountryAtributtes CA;
+
+    private void Awake() {
+        PU = GameObject.FindGameObjectWithTag("PopUpManager").GetComponent<PopUp>();
+        CA = this.transform.parent.GetComponent<CountryAtributtes>();
+        StartCoroutine(selfDestruct());
+    }
+
+    IEnumerator selfDestruct() {
+        yield return new WaitForSeconds(10f);
+        die();
+    }
+
+    void die() {
+        if (PopType == 0 && CA.prov == ("9gag")) {
+            //perde pontos
+        }
+        if (PopType == 1 && CA.prov == ("4chan")) {
+            //perde pontos
+        }
+        if (PopType == 2 && CA.prov == ("reddit")) {
+            //perde pontos
+        }
+        CA.HasPopUp = false;
+        Destroy(this.gameObject);
+    }
+
+    public void Detonate() {
+        if (PopType == 0) {
+            PU.Nup(10);
+            //if major bonus coins else more cure
+        }
+        if (PopType == 1) {
+            PU.Fup(10);
+            //if major bonus coins else more cure
+        }
+        if (PopType == 2) {
+            PU.Rup(10);
+            //if major bonus coins else more cure
+        }
+        if (PopType == 3) {
+            //abre janela meme
+        }
+        CA.HasPopUp = false;
+        Destroy(this.gameObject);
+    }
+}
